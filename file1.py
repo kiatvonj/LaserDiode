9a3b109fc935fcd18d82db2c56916bf2c7899cd6
schonsberg <sjschonsberg@gmail.com>
2021-08-18 11:44:28 -0700

    8/18 part 1

--- file1.py
+++ file1.py
@@ -14,25 +14,35 @@ DMM = rm.open_resource(DMM_name)
 print(SMU.query('*IDN?'))
 print(DMM.query('*IDN?')) # this WOULD NOT work with the Fluke DMM, switched to RIGOL
 
-
-SMU.write('SOUR:FUNC:MODE CURR')
-SMU.write('SOUR:CURR 0.010')
-# SMU.write('FORM:ELEM:SENS RES, STAT')
-SMU.write('OUTP ON')
-# SMU.write('SENS:FUNC:ALL')
-# SMU.write('SENS:FUNC:""VOLT""')
-# SMU.write('SENS:FUNC:""CURR""')
-# SMU.write('SENS:RES:OCOM ON')
-
-SMU.write('MEAS:VOLT?')
-SMU.write('initiate')
+# SMU.write('OUTP ON')                # Turns output on
+SMU.write('SENS:REM ON')            # Connection type: 4-wire mode
+SMU.write('SOUR:FUNC:MODE CURR')    # Output source function mode: current
+SMU.write('SOUR:CURR 0.030')        # Sets current: 30 mA
+SMU.write('SENS:VOLT:PROT 5.0')     # Sets compliance voltage: 5.0 V
 
 
-# print('SMU VOLT:', SMU.read('MEAS:VOLT?'))
+print('SMU Curr:', float(SMU.query('MEAS:CURR:DC?')))
+print('SMU Volt:', float(SMU.query('MEAS:VOLT:DC?')))
+
+
+
+# SMU.write('SOUR:CURR 0.0')
+
+# print(SMU.query('MEAS:VOLT:DC?'))
+# print(SMU.query('MEAS:CURR:DC?'))
+
+
+SMU.write('INIT')
+# SMU.write('DISP:ENAB ON')
+
+
+SMU.write('OUPT OFF')
+
+
 
 '''
 DMM Commands:
     DMM.query('MEAS:VOLT:DC?')
 '''
 
-print(float(DMM.query('MEAS:VOLT:DC?')))
+print('DMM Volt:', float(DMM.query('MEAS:VOLT:DC?')))
